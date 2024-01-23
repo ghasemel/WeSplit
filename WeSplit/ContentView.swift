3226b3388f9b57a9bb8e2561c6ad5ee471b610b6
Ghasem Elyasi <ghasem.elyasi@swisscom.com>
2024-01-23 10:02:35 +0100

    hide decimal keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
+    @FocusState private var amountIfFocused: Bool
     
     private let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -31,6 +32,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIfFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -55,7 +57,15 @@ struct ContentView: View {
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
-            }.navigationTitle("WeSplit")
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIfFocused {
+                    Button("Done") {
+                        amountIfFocused = false
+                    }
+                }
+            }
         }
     }
 }
